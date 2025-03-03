b6d375c81fb2ae0fbb2a0a1ee68dc387444d0148
Aellas <zacpi@pm.me>
2025-03-03 19:32:38 +0000

    Revert "laptop version"

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,9 +1,8 @@
 
+xrandr --output DisplayPort-0 --mode 1920x1080 --rate 239.76 --dpi 99
 /usr/libexec/polkit-gnome-authentication-agent-1 &
 /usr/bin/dunst &
 picom --daemon --config $HOME/.config/qtile/scripts/picom.conf &
 #feh --bg-fill /home/array/Pictures/orange.jpg &
-setxkbmap gb &
 pipewire &
-devmon &
 xclip &
